a3149347fe0394e6302e240fbc202c3771725b23
Jed Tiotuico <jed.tiotuico@gmail.com>
2015-08-09 11:09:47 +0800

    fixed ios

--- app/config/AppConfig.js
+++ app/config/AppConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  IS_DEVELOPMENT: true,
+  IS_DEVELOPMENT: false,
   APP_ID: 'F7wnLsTNp6SEEZmoDpTsEMwz63gIiqkzx8sDnDva',
   MASTER_KEY: 'PK7iDBROBQF4s8EWPvBtknZkyqOkWK4kjf3JpPSS',
   JS_KEY: '0WjWsjZdQWuJEtEYz0OnI02Se7QIAm4S14fZ4Nlf'
